617bcb8fc7eb69cc84153aff118b853e8ae75243
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2021-11-24 15:42:13 +0100

    Dodavanje podataka za djelatnike i korisnike

--- zavrsnirad.sql
+++ zavrsnirad.sql
@@ -52,3 +52,23 @@ alter table korisnik_usluga add foreign key(korisnik) references korisnik(sifra)
 
 alter table posjeta_usluga add foreign key(posjeta) references posjeta(sifra);
 alter table posjeta_usluga add foreign key(usluga) references usluga(sifra);
+
+insert into korisnik(sifra,ime,prezime,oib,datumrodenja) values
+(null,'Zoran','Horvat','83666361444',"1984-10-24"),
+(null,'Ivan','Stankić','95891366101',null),
+(null,'Andrija','Jugovac','77894844566',"1990-03-05"),
+(null,'Ante','Đurić',null,null),
+(null,'Marija','Kovačić','22289340362',"1975-12-24"),
+(null,'Bojana','Lončar','26551968464',"1986-11-23"),
+(null,'Krunoslav','Kovačević',null,"1979-10-22"),
+(null,'Tena','Horvat','65133628998',null);
+
+insert into djelatnik(sifra,ime,prezime,uloga,email,placa) values
+(null,'Bernard','Pavlović','Šef recepcije','bernardpavlovic@gmail.com',8000.00),
+(null,'Roman','Janković','Zamjenik šefa recepcije',null,6800.00),
+(null,'Tonka','Petrović','Recepcionarka',null,null),
+(null,'Ante','Vukoja','Recepcionar',null,null),
+(null,'Gordana','Radić','Recepcionarka',null,5750.00),
+(null,'Jolanda','Lončar','Pomoćna recepecionarka','jolandaloncar@gmail.com',5000.00),
+(null,'Antonio','Kolarić','Recepcioinar','antoniokolaric@gmail.com',5800.00),
+(null,'Dubravko','Horvat','Noćni recepcionar',null,null);
